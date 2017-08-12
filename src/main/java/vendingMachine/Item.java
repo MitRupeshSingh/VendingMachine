f60f45b249a765e3fcb16a34c45c6f6b9a6a0630
Rupesh Singh <rupeshsinghmit@gmail.com>
2017-08-12 12:00:52 -0400

    add all the files

---
+++ src/main/java/vendingMachine/Item.java
@@ -0,0 +1,17 @@
+package vendingMachine;
+
+//Item
+public enum Item{ 
+	COKE("COKE", 100), CANDY("CANDY", 65), CHIPS("CHIPS", 50); 
+	private String name; 
+	private int price; 
+	private Item(String name, int price){ 
+		this.name = name; this.price = price; 
+	} 
+	public String getName(){ 
+		return name; 
+	} 
+	public int getPrice(){ 
+		return price; 
+	} 	
+}
